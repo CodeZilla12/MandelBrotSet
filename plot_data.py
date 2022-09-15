18f126fc12c8b89a8f84324e24e2b7bd16c95f7b
SGL <samgledhill227@gmail.com>
2022-09-15 07:30:16 +0100

    Computation now done using async to compute all rows simultaneously, leads to ~12x speedup.

--- plot_data.py
+++ plot_data.py
@@ -6,9 +6,11 @@ with open("mbdata.txt") as f:
     data = f.read()
     lines = [list(map(int,i.split(","))) for i in data.split("\n")[1:] if i] #parses all lines from file and cast as int
 
-rmin,rmax,imin,imax = [float(i) for i in data.split("\n")[0].split(",")]
+rmin,rmax,imin,imax,depth = [float(i) for i in data.split("\n")[0].split(",")]
 
+
+plt.imshow(np.rot90(lines),extent=[rmin,rmax,imin,imax], aspect = 1, vmin = 0, vmax = depth)
 plt.xlabel("Real")
 plt.ylabel("Imag")
-plt.imshow(np.rot90(lines),extent=[rmin,rmax,imin,imax], aspect = 1, vmin = 0, vmax = 80)
+plt.colorbar(label = "# Iterations")
 plt.show()
